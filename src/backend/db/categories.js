0a7375cafac1128450a2441c4baef333b27cfdb7
ritikaawasthy <ritika.awasthy@gmail.com>
2022-03-28 20:39:20 +0530

    added products and categories

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -9,22 +9,28 @@ export const categories = [
   {
     _id: uuid(),
     categoryName: "Topwear",
-    picture: 'https://storyset.com/illustration/city-girl/bro#65012EFF&hide=Car,City,Clouds,traffic-light,fire-hydrant,Floor&hide=simple',
+    picture: 'https://github.com/ritikaawasthy/react-ecommerce/blob/components/public/media/category/top.webp',
   },
   {
     _id: uuid(),
     categoryName: "Bottomwear",
-    picture: 'https://storyset.com/illustration/waist-bag/bro#65012EFF&hide=Clouds,speech-bubble,Line,Arrow&hide=simple' ,
+    picture: 'https://github.com/ritikaawasthy/react-ecommerce/blob/components/public/media/category/bottom.webp' ,
   },
   {
     _id: uuid(),
     categoryName: "Footwear",
-    picture: 'https://storyset.com/illustration/waist-bag/bro#65012EFF&hide=Clouds,speech-bubble,Line,Arrow&hide=simple' ,
+    picture: 'https://github.com/ritikaawasthy/react-ecommerce/blob/components/public/media/category/shoes.jpg' ,
   },
   {
     _id: uuid(),
     categoryName: "Accessories",
-    picture: 'https://storyset.com/illustration/waist-bag/bro#65012EFF&hide=Clouds,speech-bubble,Line,Arrow&hide=simple' ,
+    picture: 'https://github.com/ritikaawasthy/react-ecommerce/blob/components/public/media/category/accessories.webp' ,
   },
 
 ];
+
+
+
+
+// https://storyset.com/illustration/city-girl/bro#65012EFF&hide=Car,City,Clouds,traffic-light,fire-hydrant,Floor&hide=simple
+// https://storyset.com/illustration/waist-bag/bro#65012EFF&hide=Clouds,speech-bubble,Line,Arrow&hide=simple

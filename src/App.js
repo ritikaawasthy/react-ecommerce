01a4d546e42e1a425a7d538607feead4ad5ca356
ritikaawasthy <ritika.awasthy@gmail.com>
2022-03-31 18:18:00 +0530

    ressolved router error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import {SignupPage} from "./pages/index.js";
 import {
-  BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
@@ -10,12 +9,10 @@ function App() {
     <div className="App">
       <header className="App-header">
       </header>
-      <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />}></Route>
           <Route path="/signup" element={<SignupPage />}></Route>
         </Routes>
-      </BrowserRouter>
     </div>
   );
 }

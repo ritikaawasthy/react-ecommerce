c84349bc50ee8dae46c43e70ca84fc56cc7d30dd
ritikaawasthy <ritika.awasthy@gmail.com>
2022-03-31 18:19:59 +0530

    router error ressolved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import {SignupPage, LoginPage} from "./pages/index.js";
 import {
-  BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
@@ -10,13 +9,11 @@ function App() {
     <div className="App">
       <header className="App-header">
       </header>
-      <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />}></Route>
           <Route path="/signup" element={<SignupPage />}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
         </Routes>
-      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from "./App";
 import { makeServer } from "./server";
 import "./styles/styles.css";
 import {AuthProvider} from "./context/authContext";
+import {BrowserRouter} from "react-router-dom";
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
     <AuthProvider>
     <App/>
     </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
